8e228ebc967f249aa0acb7d925776a4f7777d1bd
Xavier <pedrooohhenriquex@hotmail.com>
2024-05-17 21:56:00 -0300

    Xavier dos pares

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Números pares entre 0 e ? ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+for(int x = 0; x <= numero ; x+=2)
+{
+    Console.Write($"{x} ");
+
+}
